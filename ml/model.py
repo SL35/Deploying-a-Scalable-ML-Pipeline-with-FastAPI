852ffe019f1e24d0531ebcaea4af67605052e9cf
SamrL <sleav35@wgu.edu>
2025-03-01 14:17:29 -0800

    Implemented model.py functions (untested).

--- ml/model.py
+++ ml/model.py
@@ -1,7 +1,9 @@
 import pickle
 from sklearn.metrics import fbeta_score, precision_score, recall_score
 from ml.data import process_data
-# TODO: add necessary import
+from sklearn.linear_model import LogisticRegression
+from data import process_data
+
 
 # Optional: implement hyperparameter tuning.
 def train_model(X_train, y_train):
@@ -20,7 +22,9 @@ def train_model(X_train, y_train):
         Trained machine learning model.
     """
     # TODO: implement the function
-    pass
+    lr = LogisticRegression(max_iter=1000)
+    lr.fit(X_train, y_train)
+    return lr
 
 
 def compute_model_metrics(y, preds):
@@ -50,7 +54,7 @@ def inference(model, X):
 
     Inputs
     ------
-    model : ???
+    model : no type
         Trained machine learning model.
     X : np.array
         Data used for prediction.
@@ -60,7 +64,9 @@ def inference(model, X):
         Predictions from the model.
     """
     # TODO: implement the function
-    pass
+    preds = model.predict(X)
+    return preds
+
 
 def save_model(model, path):
     """ Serializes model to a file.
@@ -72,20 +78,22 @@ def save_model(model, path):
     path : str
         Path to save pickle file.
     """
-    # TODO: implement the function
+
+    pickle.dump(model, open(path, 'wb'))
     pass
+
 
 def load_model(path):
     """ Loads pickle file from `path` and returns it."""
-    # TODO: implement the function
-    pass
+
+    model = pickle.load(open(path, 'rb'))
+    return model
 
 
 def performance_on_categorical_slice(
-    data, column_name, slice_value, categorical_features, label, encoder, lb, model
+        data, column_name, slice_value, categorical_features, label, encoder, lb, model
 ):
     """ Computes the model metrics on a slice of the data specified by a column name and
-
     Processes the data using one hot encoding for the categorical features and a
     label binarizer for the labels. This can be used in either training or
     inference/validation.
@@ -118,11 +126,12 @@ def performance_on_categorical_slice(
 
     """
     # TODO: implement the function
-    X_slice, y_slice, _, _ = process_data(
-        # your code here
-        # for input data, use data in column given as "column_name", with the slice_value 
-        # use training = False
-    )
-    preds = None # your code here to get prediction on X_slice using the inference function
+    X_slice, y_slice, _, _ = process_data((data[column_name] == slice_value),
+                                          categorical_features=[categorical_features],
+                                          label=label,
+                                          training=False,
+                                          encoder=encoder,
+                                          lb=lb)
+    preds = model.predict(X_slice)
     precision, recall, fbeta = compute_model_metrics(y_slice, preds)
     return precision, recall, fbeta
